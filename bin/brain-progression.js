07bc8bf0e4df3af52a4bb368388806b53e802f93
Dmitry Chmerenko <dchmerenko@gmail.com>
2022-02-04 09:16:43 +0300

    Add brain-progression game.

---
+++ bin/brain-progression.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line no-unused-vars
+import name from '../src/cli.js';
+import askProgression from '../games/progression.js';
+
+askProgression();
---
+++ games/progression.js
@@ -0,0 +1,30 @@
+import name from '../src/cli.js';
+import { doQuiz, randInt } from '../src/index.js';
+
+const generateProgressionQuestion = () => {
+  const start = randInt();
+  const step = randInt(2, 10);
+  const repeat = randInt(5, 10);
+  const progression = [];
+
+  for (let i = start; i < step * repeat; i += step) {
+    progression.push(String(i));
+  }
+
+  const secretIdx = randInt(repeat - 1);
+  const correctAnswer = progression[secretIdx];
+  progression[secretIdx] = '..';
+  const sequence = progression.join(' ');
+
+  return [sequence, correctAnswer];
+};
+
+const taskMsg = 'What number is missing in the progression?';
+
+const askProgression = () => doQuiz(
+  name,
+  taskMsg,
+  generateProgressionQuestion,
+);
+
+export default askProgression;
