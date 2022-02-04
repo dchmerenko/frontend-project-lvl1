f057675fc97f7cb88c494bfc3c3152c0b53af7cd
Dmitry Chmerenko <dchmerenko@gmail.com>
2022-02-04 11:09:08 +0300

    Fix isPrime.
    Fix prime number calculation.

--- src/index.js
+++ src/index.js
@@ -50,6 +50,9 @@ const isEven = (number) => number % 2 === 0;
  * Return true if number is prime else false.
  */
 const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       return false;
